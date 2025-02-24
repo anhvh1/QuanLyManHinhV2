6ceeaf4c624a10d1b86aa9caed05f7b4dc229ca7
Nguyen Minh <nguyenminh18ntnd@gmail.com>
2025-02-24 10:49:07 +0700

    figbug

--- src/customApp/containers/DanhMuc/GiaoDienKhachHang/Content/ThietBi/SlideViewer.js
+++ src/customApp/containers/DanhMuc/GiaoDienKhachHang/Content/ThietBi/SlideViewer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as signalR from "@microsoft/signalr";
 import {
   _debounce,
